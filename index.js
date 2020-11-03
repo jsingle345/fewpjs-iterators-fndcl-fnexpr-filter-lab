0a7e2d52ab60d876a6ba73d67ce08aedf55b4070
jsingle345 <jsingle345@gmail.com>
2020-11-03 09:21:26 -0600

    commit tree b5a8237f918b0d3cb2e7e104b6f44b0235359d12

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+function findMatching(drivers, name){
+    let sameName = drivers.filter(driver => 
+      driver.toLowerCase() == name.toLowerCase()
+    )
+    return sameName;
+}
+
+function fuzzyMatch(drivers, name){
+    let similarName = drivers.filter(driver => 
+        driver.charAt(0,1,2) === name.charAt(0,1,2)
+        )
+    return similarName
+}   
+
+function matchName(drivers, string){
+    let sameObject = drivers.filter(driver => 
+       driver["name"] === string
+        )
+    return sameObject
+}
